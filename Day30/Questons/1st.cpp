8208b6a4e1c1f35bdd1a058619d6aebf6e4744d8
krishna9809 <multiatoms980996@gmail.com>
2023-09-24 12:38:58 +0530

    Day30 with DSA

---
+++ Day30/Questons/1st.cpp
@@ -0,0 +1,60 @@
+// EKO EKO - SPOJ
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool isPossibleSolution(vector<long long int> trees, long long int m, long long int mid)
+{
+    long long int woodCollected = 0;
+
+    for (int i = 0; i < trees.size(); i++)
+    {
+        if (trees[i] > mid)
+        {
+            woodCollected += trees[i] - mid;
+        }
+    }
+    return woodCollected >= m;
+}
+
+int maxSawBladeHeight(vector<long long int> trees, long long int m)
+{
+    long long start, end, ans = -1;
+    start = 0;
+    end = *max_element(trees.begin(), trees.end());
+
+    while (start <= end)
+    {
+        long long int mid = start + (end - start) / 2;
+        if (isPossibleSolution(trees, m, mid))
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    long long int n, m;
+    cin >> n >> m;
+
+    vector<long long int> trees;
+
+    while (n--)
+    {
+        long long int height;
+        cin >> height;
+        trees.push_back(height);
+    }
+
+    cout << maxSawBladeHeight(trees, m) << endl;
+}
