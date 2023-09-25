c70b6cac483fb259725372855c0d6318e492395c
krishna9809 <multiatoms980996@gmail.com>
2023-09-26 04:38:09 +0530

    Day31

---
+++ Day31/Char-array/1st.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // char name[100];
+    // cout<<"Enter your Name"<<endl;
+    // cin>>name;
+    // cout<<"Aapka name "<<name <<" hai "<<endl;
+
+    // char name[100];
+
+    // cin>> name;
+    // for(int i = 0;i<8;i++){
+    //     cout<<"index: " <<i<<" value: "<< name[i] <<endl;
+    // }
+
+    // int value = (int)name[7];
+    // cout<<value;
+
+    char name[100];
+
+    cin>>name;
+
+    cout<<name;
+}
