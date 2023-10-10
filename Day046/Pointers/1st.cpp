38cb8de24ef2890d3f8432f8276255cb05bcc98d
krishna9809 <multiatoms980996@gmail.com>
2023-10-10 06:16:05 +0530

    DAY46 with DSA

---
+++ Day046/Pointers/1st.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int p = 5;
+
+    // int* c = &p;
+
+    // cout<<*c;
+
+    // int* ptr = 0;
+    // cout<<*ptr<<endl;
+    // int*ctr = nullptr;
+
+    // int a = 10;
+    // int* p = &a;
+    // int*q = p;
+    // int* r = q;
+    
+    int a = 10;
+    int ptr = 208;
+
+    int* ptr = &a;
+
+
+    cout<<a<<endl<<endl;
+    cout<<&a<<endl<<endl;
+    cout<<ptr<<endl<<endl;
+    // cout<<<<endl<<endl;
+    cout<<a<<endl<<endl;
+    cout<<a<<endl<<endl;
+    cout<<a<<endl<<endl;
+    cout<<a<<endl<<endl;
+    cout<<a<<endl<<endl;
+
+    
+}
