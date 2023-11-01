33576dba882756c21d3a8cffe8d2f2ef7cea55b0
krishna9809 <multiatoms980996@gmail.com>
2023-11-01 22:48:31 +0530

    Day068 with DSA

---
+++ Day068/Question/2nd.cpp
@@ -0,0 +1,24 @@
+// Stock Buy And Sell
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < arr.size(); i++) {
+        minPrice = min(minPrice, arr[i]);
+        maxPro = max(maxPro, arr[i] - minPrice);
+    }
+    
+    return maxPro;
+}
+
+int main() {
+    vector<int> arr = {7,1,5,3,6,4};
+    int maxPro = maxProfit(arr);
+    cout << "Max profit is: " << maxPro << endl;
+}
