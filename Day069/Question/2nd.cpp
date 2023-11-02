825ca2ac5a9134a6b21223e5458ee6f9c8a34dec
krishna9809 <multiatoms980996@gmail.com>
2023-11-02 21:21:25 +0530

    Day069 with DSA

---
+++ Day069/Question/2nd.cpp
@@ -0,0 +1,39 @@
+// Merge Overlapping Sub-intervals
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr) {
+    int n = arr.size(); // size of the array
+
+    //sort the given intervals:
+    sort(arr.begin(), arr.end());
+
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n; i++) {
+        // if the current interval does not
+        // lie in the last interval:
+        if (ans.empty() || arr[i][0] > ans.back()[1]) {
+            ans.push_back(arr[i]);
+        }
+        // if the current interval
+        // lies in the last interval:
+        else {
+            ans.back()[1] = max(ans.back()[1], arr[i][1]);
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<vector<int>> arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+    vector<vector<int>> ans = mergeOverlappingIntervals(arr);
+    cout << "The merged intervals are: " << "\n";
+    for (auto it : ans) {
+        cout << "[" << it[0] << ", " << it[1] << "] ";
+    }
+    cout << endl;
+    return 0;
+}
