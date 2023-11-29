75c15b885af28454dd00364306814be36b130d1b
krishna9809 <multiatoms980996@gmail.com>
2023-11-29 19:49:35 +0530

    Day092 with DSA

---
+++ Day092/Ques/1st.cpp
@@ -0,0 +1,66 @@
+/*class Solution {
+private:
+    void solve(string digit,string output,int index,vector<int>& ans, string mapping[]) {
+        
+        if(index>=digit.length()){
+            ans.push_back(output);
+            return ;
+        }
+        int number = digit[index]-'0';
+        string value = mapping[number];
+        for(int i = 0 i<value.length(); i++){
+            output.push_back(value[i]);
+            solve(digit,output,index+1,ans,mapping);
+            output.pop_back();
+        }
+ }
+
+public:
+         vector<string> letterCombinations(string digits){
+             vector<string>ans;
+             if(digit.lenth()==0)
+              return ans;
+             
+             string output;
+             int index = 0;
+             string mapping[10] = {"","","abc","def","ghi","jkl","mno","pqr","stu","uvw","xyz"};
+             solve(digit,output,index,ans,mapping);
+
+             return ans;
+
+
+         }
+};   
+*/ 
+class Solution {
+private:
+    void solve(string digit, string output, int index, vector<string>& ans, string mapping[] ) {
+        
+        //base case
+        if(index >= digit.length()) {
+            ans.push_back(output);
+            return;
+        }
+        
+        int number = digit[index] - '0';
+        string value = mapping[number];
+        
+        for(int i=0; i<value.length(); i++) {
+            output.push_back(value[i]);
+            solve(digit, output, index+1, ans, mapping);
+            output.pop_back();
+        }
+        
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.length()==0)
+            return ans;
+        string output;
+        int index = 0;
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits, output, index, ans, mapping);
+        return ans;
+    }
+};    
