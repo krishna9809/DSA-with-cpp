c6a23c97171c6c7c8803429184c62d1ab023be22
krishna9809 <multiatoms980996@gmail.com>
2023-11-30 21:58:39 +0530

    Day093 with DSA

---
+++ Day093/Ques/3rd.cpp
@@ -0,0 +1,26 @@
+// Remove Duplicates in-place from Sorted Array
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int removeDuplicates(int arr[], int n)
+{
+  int i = 0;
+  for (int j = 1; j < n; j++) {
+    if (arr[i] != arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+int main() {
+  int arr[] = {1,1,2,2,2,3,3};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  int k = removeDuplicates(arr, n);
+  cout << "The array after removing duplicate elements is " << endl;
+  for (int i = 0; i < k; i++) {
+    cout << arr[i] << " ";
+  }
+}
